a6388340d0d7c40550584fabb8e6d401d8f3124a
ShamsherAlam <same.ddps@gmail.com>
2020-06-08 11:47:07 +0530

    data in parent component created called 'data'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parent-to-child';
+  data = [
+    {title:"title 1",content:"content 1"},
+    {title:"title 2",content:"content 2"},
+    {title:"title 3",content:"content 3"},
+    {title:"title 4",content:"content 4"},
+    {title:"title 5",content:"content 5"}
+  ];
 }
